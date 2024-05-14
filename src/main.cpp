32b5b9d969b4c16b1fd47c89d0e8aeaae13339ab
Jakub Guzewicz <jakub.guzewicz@gmail.com>
2024-05-14 21:58:11 +0200

    A little more client listener and cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include "main.hpp"
 #include <iostream>
+#include <sys/socket.h>
+#include <sys/types.h>
 #include <thread>
 
 int main(int argc, char const *argv[]) {
@@ -43,6 +45,44 @@ void listen_for_new_clients_ssl(std::string port,
     std::cout << "TODO: setup client listener";
     (void)port;
     (void)users_vector;
+
+    // TODO: change paths to use config file
+    auto cert_path = "certs/client_cert.pem";
+    auto key_path = "certs/client_key.pem";
+
+    auto dtls_ctx = SSL_CTX_new(DTLS_server_method());
+    if (dtls_ctx == NULL) {
+        throw std::runtime_error("Failed to create SSL_CTX");
+    }
+
+    SSL_CTX_set_min_proto_version(dtls_ctx, DTLS1_2_VERSION);
+
+    if (!SSL_CTX_use_certificate_file(dtls_ctx, cert_path, SSL_FILETYPE_PEM)) {
+        throw std::runtime_error("Failed to load client cert with path: " +
+                                 std::string(cert_path));
+    }
+
+    if (!SSL_CTX_use_PrivateKey_file(dtls_ctx, key_path, SSL_FILETYPE_PEM)) {
+        throw std::runtime_error("Failed to load client key with path: " +
+                                 std::string(key_path));
+    }
+
+    // It should be a loop from here
+
+    auto dtls_ssl = SSL_new(dtls_ctx);
+    if (dtls_ssl == NULL) {
+        throw std::runtime_error("Failed to create client listener SSL");
+    }
+
+    // TODO: check if needed in mock
+
+    SSL_set_options(dtls_ssl, SSL_OP_COOKIE_EXCHANGE);
+
+    auto sock = BIO_socket(AF_INET, SOCK_DGRAM, 0, 0);
+    if (sock < 0) {
+        throw std::runtime_error(
+            "Could not create socket fd for client listener");
+    }
 }
 
 void listen_for_new_game_servers_ssl(
