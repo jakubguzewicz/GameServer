a146142cf962c2f6514e93578b28d5b087e7ce70
Jakub Guzewicz <jakub.guzewicz@gmail.com>
2024-05-14 19:38:10 +0200

    Decoupled auth and game servers threads

--- include/main.hpp
+++ include/main.hpp
@@ -9,6 +9,7 @@ int main(int argc, char const *argv[]);
 
 void listen_for_new_clients_ssl(std::string port,
                                 std::vector<UserSession> &users_vector);
-void listen_for_new_servers_ssl(std::string port,
-                                std::vector<GameServer> &game_servers_vector,
-                                std::vector<AuthServer> &auth_servers_vector);
+void listen_for_new_game_servers_ssl(
+    std::string port, std::vector<GameServer> &game_servers_vector);
+void listen_for_new_auth_servers_ssl(
+    std::string port, std::vector<AuthServer> &auth_servers_vector);
--- src/main.cpp
+++ src/main.cpp
@@ -7,27 +7,32 @@ int main(int argc, char const *argv[]) {
     (void)argc;
 
     // Setup empty data structures
-    std::vector<GameServer> gameServers;
-    std::vector<AuthServer> authServers;
-    std::vector<UserSession> connectedUsers;
+    std::vector<GameServer> game_servers;
+    std::vector<AuthServer> auth_servers;
+    std::vector<UserSession> connected_users;
 
     // Setup listeners
     std::cout << "Hello there!" << std::endl;
 
     // TODO: change to read from config file
     std::string client_port = "4720";
-    std::string server_port = "4721";
+    std::string auth_server_port = "4721";
+    std::string game_server_port = "4722";
 
     std::thread clients_listener_thread(listen_for_new_clients_ssl, client_port,
-                                        std::ref(connectedUsers));
+                                        std::ref(connected_users));
 
-    std::thread servers_listener_thread(listen_for_new_servers_ssl, server_port,
-                                        std::ref(gameServers),
-                                        std::ref(authServers));
+    std::thread auth_servers_listener_thread(listen_for_new_auth_servers_ssl,
+                                             auth_server_port,
+                                             std::ref(auth_servers));
+
+    std::thread game_servers_listener_thread(listen_for_new_game_servers_ssl,
+                                             game_server_port,
+                                             std::ref(game_servers));
 
     clients_listener_thread.join();
-    servers_listener_thread.join();
-    std::cout << "Something went very, very, wrong if you reached here (both "
+    auth_servers_listener_thread.join();
+    std::cout << "Something went very, very, wrong if you reached here (all "
                  "listener threads ended execution)."
               << std::endl;
 }
@@ -40,12 +45,18 @@ void listen_for_new_clients_ssl(std::string port,
     (void)users_vector;
 }
 
-void listen_for_new_servers_ssl(std::string port,
-                                std::vector<GameServer> &game_servers_vector,
-                                std::vector<AuthServer> &auth_servers_vector) {
+void listen_for_new_game_servers_ssl(
+    std::string port, std::vector<GameServer> &game_servers_vector) {
 
     std::cout << "TODO: setup server listener" << std::endl;
     (void)port;
     (void)game_servers_vector;
+}
+
+void listen_for_new_auth_servers_ssl(
+    std::string port, std::vector<AuthServer> &auth_servers_vector) {
+
+    std::cout << "TODO: setup auth server listener" << std::endl;
+    (void)port;
     (void)auth_servers_vector;
 }
